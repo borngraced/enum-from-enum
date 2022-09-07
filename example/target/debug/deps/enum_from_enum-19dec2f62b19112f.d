ff2387f88533357204e77464622630d13e9ea2b5
borngraced <samiodev@icloud.com>
2022-09-07 14:33:42 +0000

    added error message

--- example/target/debug/deps/enum_from_enum-19dec2f62b19112f.d
+++
@@ -1,5 +0,0 @@
-/home/sami/projects/enum-from-enum/example/target/debug/deps/libenum_from_enum-19dec2f62b19112f.so: /home/sami/projects/enum-from-enum/src/lib.rs
-
-/home/sami/projects/enum-from-enum/example/target/debug/deps/enum_from_enum-19dec2f62b19112f.d: /home/sami/projects/enum-from-enum/src/lib.rs
-
-/home/sami/projects/enum-from-enum/src/lib.rs:
